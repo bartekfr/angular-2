85050fd3ea22ca470813d88fa6f0080a011eeb24
Bartek Fryzowicz <bartekfr@wp.pl>
2016-09-20 18:32:40 +0200

    init

---
+++ README.md
@@ -0,0 +1,11 @@
+# Angular 2 sample app
+
+Application based on [Angular Class](http://courses.angularclass.com/) course
+
+### Usage
+- Make sure you have [node.js](https://nodejs.org/) installed version 5+
+- Make sure you have NPM installed version 3+
+- run `npm install -g webpack webpack-dev-server typings typescript ts-node tslint`
+- run `npm install`
+- run `npm start` to fire up dev server
+- open browser to [`http://localhost:3000`](http://localhost:3000)
---
+++ src/app/containers/notes.ts
@@ -0,0 +1,46 @@
+import { Component } from '@angular/core';
+import { NoteCard } from '../ui';
+
+@Component({
+	selector: 'notes-container',
+	directives: [
+	NoteCard
+	],
+	styles: [`
+	.notes {
+		padding-top: 50px;
+	}
+	.creator {
+		margin-bottom: 40px;
+	}
+	`],
+	template: `
+	<div class="row center-xs notes">
+		<div class="col-xs-6 creator">
+		note creator here
+		</div>
+		<div class="notes col-xs-8">
+		<div class="row between-xs">
+			<note-card
+			class="col-xs-4"
+			(checked)="onNoteChecked($event, i)"
+			*ngFor="let note of notes; let i = index"
+			[note]="note"
+			>
+			</note-card>
+		</div>
+		</div>
+	</div>
+	`
+})
+export class Notes {
+	notes = [
+	{title: 'Chores 1', value: 'Don\'t forget to clean up', color: '#eee'},
+	{title: 'Chores 2', value: 'Don\'t forget to clean up', color: '#ddd'},
+	{title: 'Chores 3', value: 'Don\'t forget to clean up', color: '#eee'}];
+
+	onNoteChecked(note, i) {
+		console.log(note)
+		this.notes.splice(i, 1)
+	}
+}
